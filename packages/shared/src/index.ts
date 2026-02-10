c01536aebeb8dbab712ef53034a9c1fc9cdd49be
Enrique Meneses <109693762+emnss26@users.noreply.github.com>
2026-02-10 11:08:01 -0600

    Auth out of issues

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,15 @@ export * from "./aps/oauth.js";
 export * from "./aps/types.js";
 export * from "./aps/session.js";
 
+export {
+  beginLogin,
+  completeLogin,
+  getAuthStatus,
+  logout,
+  ensureAccessToken
+} from "./auth/authManager.js";
+
+export { getContext, setContext } from "./config/contextStore.js";
+
 export { getAccAccessToken } from "./aps/oauth.js";
 
